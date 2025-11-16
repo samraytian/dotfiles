60804080a8abbe9df2913a72ae69fa957dbec073
Sam <samray.tian@gmail.com>
2025-11-16 16:18:15 +0800

    feat(setup): remove gitignore and streamline setup script

--- setup.sh
+++ setup.sh
@@ -6,14 +6,7 @@ set -e
 stow -R -v zsh --target="$HOME"
 
 # git
-if [ -f "$HOME/.gitconfig" ]; then
-  rm "$HOME/.gitconfig"
-fi
-if [ -f "$HOME/.gitignore" ]; then
-  rm "$HOME/.gitignore"
-fi
-ln -s "$HOME/.dotfiles/git/.gitconfig" "$HOME/.gitconfig"
-ln -s "$HOME/.dotfiles/git/.gitignore" "$HOME/.gitignore"
+stow -R -v git --target="$HOME"
 
 # starship
 stow -R -v starship --target="$HOME"
