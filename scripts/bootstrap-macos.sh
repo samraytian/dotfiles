9ca29274b6531a5c5d1674a17fb7fcb9b3dab90f
Sam <samray.tian@gmail.com>
2026-01-31 02:17:18 +0800

    refactor(macos): streamline package installation checks in bootstrap script

--- scripts/bootstrap-macos.sh
+++ scripts/bootstrap-macos.sh
@@ -2,54 +2,30 @@
 
 set -e
 
-# if homwbrew is not installed, exit
 [ -x "$(command -v brew)" ] || exit 1
 
-# stow
-[ -x "$(command -v stow)" ] || brew install stow
+packages=(
+  "stow"
+  "tmux"
+  "bat"
+  "eza"
+  "cmake"
+  "ripgrep"
+  "fd"
+  "fzf"
+  "zoxide"
+  "shellcheck"
+  "tree-sitter"
+  "tree-sitter-cli"
+  "gh"
+  "git-flow"
+  "thefuck"
+  "starship"
+  "zsh-completions"
+  "zsh-autosuggestions"
+  "zsh-syntax-highlighting"
+)
 
-# tmux
-[ -x "$(command -v tmux)" ] || brew install tmux
-
-# bat, an cat clone with syntax highlighting and Git integration
-[ -x "$(command -v bat)" ] || brew install bat
-
-# eza, a replacement for ls
-[ -x "$(command -v eza)" ] || brew install eza
-
-# ripgrep, a line-oriented search tool that recursively searches the current directory for a regex pattern
-[ -x "$(command -v rg)" ] || brew install ripgrep
-
-# fd, a simple, fast and user-friendly alternative to find
-[ -x "$(command -v fd)" ] || brew install fd
-
-# fzf, a command-line fuzzy finder
-[ -x "$(command -v fzf)" ] || brew install fzf
-
-# zoxide, a smarter cd command
-[ -x "$(command -v zoxide)" ] || brew install zoxide
-
-# tree-sitter and tree-sitter-cli
-[ -x "$(command -v tree-sitter)" ] || brew install tree-sitter
-[ -x "$(command -v tree-sitter-cli)" ] || brew install tree-sitter-cli
-
-# gh
-[ -x "$(command -v gh)" ] || brew install gh
-
-# thefuck
-[ -x "$(command -v thefuck)" ] || brew install thefuck
-
-# starship
-[ -x "$(command -v starship)" ] || brew install starship
-
-# git-flow
-[ -x "$(command -v git-flow)" ] || brew install git-flow
-
-# zsh-completions
-[ -d "$(brew --prefix)/share/zsh-completions" ] || brew install zsh-completions
-
-# zsh-autosuggestions
-[ -d "$(brew --prefix)/share/zsh-autosuggestions" ] || brew install zsh-autosuggestions
-
-# zsh-syntax-highlighting
-[ -d "$(brew --prefix)/share/zsh-syntax-highlighting" ] || brew install zsh-syntax-highlighting
+for package in "${packages[@]}"; do
+  [ -x "$(command -v "$package")" ] || brew install "$package"
+done
