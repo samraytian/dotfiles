f827b0b087ae168564f7de113ede4b327e8a86c0
Sam <samray.tian@gmail.com>
2026-08-16 17:41:28 +0800

    chore: unify script shebangs

--- scripts/zsh-plugins.zsh
+++ scripts/zsh-plugins.zsh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+
 # Shared by .zshrc and install-zsh-plugins.sh.
 # Bump the version when the plugin list changes so the installer can be rerun.
 typeset -g DOTFILES_ZSH_PLUGINS_VERSION=1
