a6900bacb170ddebfc848969268ccd0b1dac723b
Sam <samray.tian@gmail.com>
2026-09-12 03:45:31 +0800

    feat(nvim): add transparent TokyoNight night theme

--- packages/nvim/lua/plugins/init.lua
+++ packages/nvim/lua/plugins/init.lua
@@ -2,6 +2,10 @@ vim.pack.add({
   -- File navigation
   "https://github.com/stevearc/oil.nvim",
   "https://github.com/refractalize/oil-git-status.nvim",
+
+  -- Appearance
+  "https://github.com/folke/tokyonight.nvim",
 })
 
+require("plugins.colorscheme")
 require("plugins.oil")
