ee6db40f2fea6a81dadcb2282e26240400e07e3f
Sam <samray.tian@gmail.com>
2026-09-12 21:13:46 +0800

    feat(nvim): clean up unused plugins and notify on removal

--- packages/nvim/lua/plugins/init.lua
+++ packages/nvim/lua/plugins/init.lua
@@ -16,6 +16,20 @@ vim.pack.add({
   "https://github.com/nvim-lua/plenary.nvim",
 })
 
+-- Clean up plugins removed from the declarations above.
+local inactive_plugins = {}
+for _, plugin in ipairs(vim.pack.get(nil, { info = false })) do
+  if not plugin.active then
+    table.insert(inactive_plugins, plugin.spec.name)
+  end
+end
+if #inactive_plugins > 0 then
+  vim.pack.del(inactive_plugins)
+  vim.schedule(function()
+    vim.notify("Removed unused plugins: " .. table.concat(inactive_plugins, ", "), vim.log.levels.INFO)
+  end)
+end
+
 require("plugins.colorscheme")
 require("plugins.lualine")
 require("plugins.oil")
