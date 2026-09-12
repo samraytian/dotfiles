a42e445430a0ccd47fac0ff309ec3df03c3d0dd2
Sam <samray.tian@gmail.com>
2026-09-12 23:05:13 +0800

    feat(nvim): map double leader to file search

--- packages/nvim/lua/plugins/telescope.lua
+++ packages/nvim/lua/plugins/telescope.lua
@@ -14,14 +14,18 @@ telescope.setup({
 telescope.load_extension("ui-select")
 telescope.load_extension("frecency")
 
+local builtin = require("telescope.builtin")
+
 local function project_root()
   return vim.fs.root(0, { ".git" }) or vim.fs.root(vim.fn.getcwd(), { ".git" }) or vim.fn.getcwd()
 end
 
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", function()
+local function find_files()
   telescope.extensions.frecency.frecency({ cwd = project_root(), workspace = "CWD", hidden = true })
-end, { desc = "Find files" })
+end
+
+vim.keymap.set("n", "<leader><leader>", find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>ff", find_files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
 vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find recent files" })
 
