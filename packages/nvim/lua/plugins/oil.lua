48d7ec4de84ab8dc2d37c3d4fdcdd152bff544fb
Sam <samray.tian@gmail.com>
2026-09-12 22:44:24 +0800

    feat(nvim): configure Oil explorer keymaps and window options

--- packages/nvim/lua/plugins/oil.lua
+++ packages/nvim/lua/plugins/oil.lua
@@ -1,6 +1,11 @@
 -- Close Oil with Ctrl-c in normal mode (default keymap).
+-- Toggle file preview with Ctrl-p in normal mode (default keymap).
 require("oil").setup({
 	default_file_explorer = true,
+
+	keymaps = {
+		["q"] = { "actions.close", mode = "n" },
+	},
 
 	columns = {
 		"icon",
@@ -14,6 +19,13 @@ require("oil").setup({
 		signcolumn = "yes:2",
 	},
 
+	float = {
+		padding = 2,
+		max_width = 0.8,
+		max_height = 0.8,
+		border = "rounded",
+	},
+
 	skip_confirm_for_simple_edits = false,
 })
 
@@ -21,4 +33,8 @@ require("oil-git-status").setup()
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", {
 	desc = "Open parent directory",
+})
+
+vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", {
+	desc = "Open file explorer",
 })
